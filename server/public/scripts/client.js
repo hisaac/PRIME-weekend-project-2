dc671fad8cea13b775599df2acc12c8880bc23bd
Isaac Halvorson <hello@hisaac.net>
2016-11-05 13:48:27 -0500

    reformat code for better readability

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,40 +2,29 @@
 
 var avatarUrl = 'http://lorempixel.com/388/388/';
 
-//---------------------------------- LOGIC -----------------------------------//
+//------------------------------ INITIALIZATION ------------------------------//
 
 $(document).ready(function(){
     init();
 });
 
-//-------------------------------- FUNCTIONS ---------------------------------//
+//---------------------------------- LOGIC -----------------------------------//
 
 function init(){
-    appendData();
+    getData();
 }
 
-function appendData(){
+//-------------------------------- FUNCTIONS ---------------------------------//
+
+// grabs data from the database and apppends it to the DOM
+function getData(){
     $.ajax({
         type: 'GET',
         url: '/data',
         success: function(response){
             for(var i = 0; i < response.sigmanauts.length; i++) {
-
-                var individual = response.sigmanauts[i];
-                // getAvatar(individual.git_username);
-
-                $('#peopleContainer').append(
-                    '<div class="person-info">' +
-                        '<h2>' + individual.name + '</h2>' +
-                        '<img class="avatar" src="' + avatarUrl + '" alt="'
-                            + individual.name + '\'s GitHub avatar"/>' +
-                        '<p>GitHub: <a href="http://github.com/' +
-                            individual.git_username + '">' +
-                            individual.git_username + '</a>' +
-                        '</p>' +
-                        '<p class="shoutout">"' + individual.shoutout + '"</p>' +
-                    '</div>'
-                );
+                // getAvatar(response.sigmanauts[i].git_username);
+                appendData(response.sigmanauts[i]);
             }
         },
         error: function() {
@@ -44,6 +33,21 @@ function appendData(){
     });
 }
 
+function appendData(individual){
+    $('#peopleContainer').append(
+        '<div class="individual-info">' +
+            '<h2>' + individual.name + '</h2>' +
+
+            '<a href="http://github.com/' + individual.git_username + '">' +
+                '<img class="avatar" src="' + avatarUrl + '" alt="' + individual.name + '\'s GitHub avatar"/>' +
+            '</a>' +
+
+            '<p class="shoutout">"' + individual.shoutout + '"</p>' +
+        '</div>'
+    );
+}
+
+// gets individual's GitHub avatar, and assigns the url to avatarUrl
 function getAvatar (username) {
     $.ajax({
         type: 'GET',
