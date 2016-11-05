dbbf4ec05b9fefb2c0ef672fcd25288650fd394e
Isaac Halvorson <hello@hisaac.net>
2016-11-05 11:39:24 -0500

    start building ajax requests

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,38 @@
+//----------------------------- GLOBAL VARIABLES -----------------------------//
+
+
+
+//---------------------------------- LOGIC -----------------------------------//
+
 $(document).ready(function(){
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        console.log(data);
-      }
-    });
+    init();
 });
+
+//-------------------------------- FUNCTIONS ---------------------------------//
+
+function init(){
+    $.ajax({
+        type: 'GET',
+        url: '/data',
+        success: function(response){
+            for(var i = 0; i < response.sigmanauts.length; i++) {
+              console.log(response.sigmanauts[i].name);
+            }
+        },
+        error: function() {
+            console.log('Error with request');
+        }
+    });
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.github.com/users/hisaac',
+        success: function(data){
+            $('#peopleContainer').html('<img src="' + data.avatar_url + '"/>');
+        },
+        error: function(){
+            console.log('Error with request');
+        }
+    })
+
+}
